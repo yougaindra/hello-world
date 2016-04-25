6cdafd29edb29d6f4bba2a95e4383cd9790f05dc
Yogendra Singh Chouhan <yougaindra@users.noreply.github.com>
2016-04-25 16:15:14 +0530

    Added files via upload

---
+++ ns.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+		int x,y;
+		scanf("%d %d",&x,&y);
+		if(x==y)
+		{
+			if(x%2)
+				printf("%d\n",x+y-1);
+			else
+				printf("%d\n",x+y);
+		}
+		else if(x == y+2)
+		{
+			if(x%2 == 0)
+				printf("%d\n",x+y);
+			else
+				printf("%d\n",x+y-1);
+		}
+		else
+			printf("No Number\n");
+	}
+	return 0;
+}
---
+++ rpn.c
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+		char q[500],ans[500];
+		scanf("%s",q);
+		int cnt =0 ;
+		stack<char> sign;
+		int i;
+		for(i=0;q[i] != '\0';i++)
+		{
+			if(q[i] =='(')
+				continue;else
+			if(q[i] >= 'a' && q[i] <= 'z')
+				ans[cnt++] = q[i];else
+			if(q[i] == '+' || q[i] == '-' || q[i] == '*' || q[i] == '/' || q[i] == '^')
+				sign.push(q[i]);
+			else if(q[i] == ')')
+			{
+				ans[cnt++] = sign.top();
+				sign.pop();
+			}
+		}
+		ans[cnt] ='\0';
+		printf("%s\n",ans);
+	}
+
+	return 0;	
+}
