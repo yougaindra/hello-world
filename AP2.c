2c15323a0eabea82fadb037bdf5352b97b22500d
Yogendra Singh Chouhan <yougaindra@users.noreply.github.com>
2016-05-05 13:36:00 +0530

    Create AP2.c

---
+++ AP2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+		long long int th, thl, sum;
+		scanf("%lld %lld %lld",&th,&thl, &sum);
+		long long int n = (2*sum)/(th+thl);
+		printf("%lld\n",n);
+		long long int d = (thl-th)/(n-5);
+		long long int a = th - 2*d;
+		long long int i;
+		for(i =0;i<n;i++)
+			printf("%lld ",a+i*d);
+		printf("\n");
+	}
+
+	return 0;
+}
